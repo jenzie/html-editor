89d6ec97b23783074dfb2df639718337b7fe81f1
Jenny Zhen <coffee@csh.rit.edu>
2013-03-22 20:20:12 -0400

    Started working on the check for valid html.

---
+++ src/se362/composite/HTMLComponent.java
@@ -0,0 +1,15 @@
+package se362.composite;
+
+public abstract class HTMLComponent {
+	private String name;
+
+	public HTMLComponent(String tag) {
+		this.name = tag.toLowerCase();
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+
+}
