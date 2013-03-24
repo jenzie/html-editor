b42928d76916626d64e820afb5c628fe3ee02b2f
Brad Bensch <bbensch@live.com>
2013-03-24 18:13:54 -0400

    Missed Adding Save class

---
+++ src/se362/command/Save.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package se362.command;
+
+/**
+ * @author Brad Bensch
+ *
+ */
+public class Save {
+
+	public void doSave(){
+		/*
+		 * Run HTMLCheck
+		 * If true, write file to new or pre-existing location
+		 */
+	}
+	
+}
