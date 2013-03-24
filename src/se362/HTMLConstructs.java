857b2dd5f30caf78a2c0828fd01720733ebeb3a2
Jenny Zhen <coffee@csh.rit.edu>
2013-03-24 17:53:38 -0400

    Added more html tags to the enums.

--- src/se362/HTMLConstructs.java
+++ src/se362/HTMLConstructs.java
@@ -7,8 +7,24 @@ package se362;
  *
  */
 public enum HTMLConstructs {
-    HTML("<html>", "</html>");
-    
+    HTML("<html>", "</html>"), HEAD("<head>","</head>"),
+	PRE("<pre>", "</pre>"),BODY("<body>", "</body>"),
+	TITLE("<title>", "</title>"), H1("<h1>", "</h1>"),
+	H2("<h2>", "</h2>"), H3("<h3>", "</h3>"), H4("<h4>", "</h4>"),
+	H5("<h5>", "</h5>"), H6("<h6>", "</h6>"), B("<b>", "</b>"),
+	I("<i>", "</i>"), TT("<tt>", "</tt>"), CITE("<cite>", "</cite>"),
+	EM("<em>", "</em>"), P("<p>", "</p>"), STRONG("<strong>", "</strong>"),
+	DL("<dl>", "</dl>"), OL("<ol>", "</ol>"),UL("<ul>", "</ul>"),
+	LI("<li>", "</li>"), TABLE("<table>", "</table>"),
+	TR("<tr>", "</tr>"), TD("<td>", "</td>"), TH("<th>", "</th>"),
+	FORM("<form>", "</form>"), FRAMESET("<frameset>", "</frameset>"),
+	NOFRAMES("<noframes>", "</noframes>"),;
+
+	/**
+	BR("<br>"), DT("<dt>"), DD("<dd>"), HR("<hr>"), FRAME("<frame>"),
+	OPTION("<option>"),
+	 */
+
     private final String openTag;
     private final String closeTag;
     
