6d4fad21cd7553a3d6dd35d94763516b2ad82194
Jenny Zhen <coffee@csh.rit.edu>
2013-03-24 22:06:47 -0400

    Added comments to the HTMLComponent. Worked on the check for valid tag; needs testing.

--- src/se362/composite/HTMLComponent.java
+++ src/se362/composite/HTMLComponent.java
@@ -1,5 +1,7 @@
 package se362.composite;
 
+import se362.HTMLConstructs;
+
 /**
  * @author Jenny Zhen, coffee@csh.rit.edu
  */
@@ -8,21 +10,41 @@ public abstract class HTMLComponent {
 	private String name;
 	private boolean isClosed;
 
+	/**
+	 * Constructor.
+	 * @param tag	html tag that may be opening/closing.
+	 */
 	public HTMLComponent(String tag) {
 		this.name = tag.toLowerCase(); // or to upper, since enums are uppercase
 	}
 
+	/**
+	 * Getter for tag value.
+	 * @return	the html tag value associated.
+	 */
 	public String getName() {
 		return this.name;
 	}
 
+	/**
+	 * Checks to see if tag was properly closed.
+	 * @return	true is tag was closed; false otherwise.
+	 */
 	public boolean isClosed() {
 		return this.isClosed;
 	}
 
+	/**
+	 * Checks to see if the given tag is a valid html tag.
+	 * (Used before adding node of the tag to the tree.
+	 * @return	true if tag is valid html; false otherwise.
+	 */
 	public boolean checkValidTag() {
-		// check against list of valid tags
-		return true;
+		// check against list of valid tags; ignore upper/lowercase
+		for (HTMLConstructs tag : HTMLConstructs.values()) {
+			if (tag.name().equalsIgnoreCase(this.name))
+				return true;
+		} return false;
 	}
 
 	// parameter might not be correct
