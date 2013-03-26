cc28768935a934e1fc199fa2ec215557753453ed
Brad Bensch <bbensch@live.com>
2013-03-26 16:08:05 -0400

    Sneaky Insert file

---
+++ src/se362/command/Insert.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package se362.command;
+
+import se362.gui.GUI;
+
+/**
+ * @author Brad Bensch
+ *
+ */
+public class Insert {
+	GUI editor;
+	
+	public Insert(GUI editor){
+		this.editor = editor;
+	}
+	
+	public void doInsert(){
+		editor.getActiveFileWindow().insertText(editor.getText());
+	}
+	
+	
+}
