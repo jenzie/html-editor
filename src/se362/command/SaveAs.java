a9011b4506e2c68b58db201e0d1735be10bd889c
Kevin <kam9115@rit.edu>
2013-03-27 20:33:36 -0400

    Fix so save as command writes to file

--- src/se362/command/SaveAs.java
+++ src/se362/command/SaveAs.java
@@ -4,6 +4,10 @@
 package se362.command;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
 
 import se362.gui.GUI;
 
@@ -20,7 +24,16 @@ public class SaveAs{
 	}
 	
 	public void doSave(){
-		editor.getFileChooser().showSaveDialog(editor); 
+	    JFileChooser fc = editor.getFileChooser();
+		int choice = fc.showSaveDialog(editor);
+		if(choice == JFileChooser.APPROVE_OPTION) {
+		    file = fc.getSelectedFile();
+		    try {
+                FileWriter writer = new FileWriter(file);
+                editor.getActiveFileWindow().getTextArea().write(writer);
+            } catch (IOException e) {
+            }
+		}
 			//Returns an Int on cancel/save/error
 	}
 }
