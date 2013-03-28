e6d21bc4c6fed37f4d7bdf4bd30b6045b9c44686
Kevin <kam9115@rit.edu>
2013-03-28 13:34:29 -0400

    Added temporary Indent, to remove errors

---
+++ src/se362/command/Indent.java
@@ -0,0 +1,15 @@
+package se362.command;
+
+public class Indent {
+
+    public void indentAll() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void indentSelected() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
