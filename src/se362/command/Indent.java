8ee585818484963deef2ed8e1eef144ebfea4a9d
William Spaw <was8908@rit.edu>
2013-04-05 16:34:10 -0400

    Fixed Indent bugs

--- src/se362/command/Indent.java
+++ src/se362/command/Indent.java
@@ -15,26 +15,40 @@ public class Indent {
 	}
 	
 	public void indentSelected(){
-		String spaces = "\n";
+		String spaces = "";
 		for (int x = 0; x<spacing; x++){
 			spaces = spaces + " ";
 		}
 		if (editor.getActiveFileWindow().getSelectedText() != null){
 			String selected = editor.getActiveFileWindow().getSelectedText();
-			selected = selected.replaceAll("\n", spaces);
+			selected = selected.replaceAll("\n", "\n" + spaces);
 			int start = editor.getActiveFileWindow().getTextArea().getSelectionStart();
 			int end = editor.getActiveFileWindow().getTextArea().getSelectionEnd();
 	    
 			editor.getActiveFileWindow().getTextArea().replaceRange(selected, start, end);
+			
+			int newLine = editor.getActiveFileWindow()
+					.getAllText().indexOf("\n");
+			
+			if (start < newLine){
+				appendText();
+			}
+			else if(newLine == -1){
+				appendText();
+			}
 		}
 		else{
 			int cursor = editor.getActiveFileWindow().getTextArea().getCaretPosition();
 			
-			String sub = editor.getActiveFileWindow().getTextArea()
-					.getText().substring(0, cursor + 1);
-			int index = sub.lastIndexOf("\n");
-			
-			editor.getActiveFileWindow().getTextArea().replaceRange(spaces, index, index + 1);
+			String subString = editor.getActiveFileWindow().getTextArea()
+					.getText().substring(0, cursor);
+			Integer index = subString.lastIndexOf("\n");
+			if (index != -1){
+				editor.getActiveFileWindow().getTextArea().replaceRange("\n" + spaces, index, index + 1);
+			}
+			else{
+				appendText();
+			}
 		}
 	}
 	
@@ -55,4 +69,16 @@ public class Indent {
 	public void setSpacing(int spacing){
 		this.spacing = spacing;
 	}
+	
+	private void appendText()
+	{
+		String spaces = "";
+		for (int x = 0; x<spacing; x++){
+			spaces = spaces + " ";
+		}
+		String text = editor.getActiveFileWindow().getAllText();
+		text = spaces + text;
+		editor.getActiveFileWindow().setTextArea(text);
+	}
+	
 }
