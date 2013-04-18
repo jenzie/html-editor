37f73a86e87d8a0be7f34de32dc3b2715d7cf0f8
Kevin <kam9115@rit.edu>
2013-04-18 17:33:47 -0400

    reformatted HTMLConstructs manually for readability

--- src/se362/HTMLConstructs.java
+++ src/se362/HTMLConstructs.java
@@ -7,19 +7,39 @@ package se362;
  * @author Jenny Zhen, coffee@csh.rit.edu
  */
 public enum HTMLConstructs {
-    HTML("<html>", "</html>"), HEAD("<head>", "</head>"), PRE("<pre>", "</pre>"), BODY(
-            "<body>", "</body>"), TITLE("<title>", "</title>"), H1("<h1>",
-            "</h1>"), H2("<h2>", "</h2>"), H3("<h3>", "</h3>"), H4("<h4>",
-            "</h4>"), H5("<h5>", "</h5>"), H6("<h6>", "</h6>"), B("<b>", "</b>"), I(
-            "<i>", "</i>"), U("<u>", "</u>"), TT("<tt>", "</tt>"), CITE(
-            "<cite>", "</cite>"), EM("<em>", "</em>"), P("<p>", "</p>"), STRONG(
-            "<strong>", "</strong>"), DL("<dl>", "</dl>"), OL("<ol>", "</ol>"), UL(
-            "<ul>", "</ul>"), LI("<li>", "</li>"), TABLE("<table>", "</table>"), TR(
-            "<tr>", "</tr>"), TD("<td>", "</td>"), TH("<th>", "</th>"), FORM(
-            "<form>", "</form>"), FRAMESET("<frameset>", "</frameset>"), NOFRAMES(
-            "<noframes>", "</noframes>"),
+    HTML("<html>", "</html>"),
+    HEAD("<head>", "</head>"),
+    PRE("<pre>", "</pre>"),
+    BODY("<body>", "</body>"),
+    TITLE("<title>", "</title>"),
+    H1("<h1>","</h1>"),
+    H2("<h2>", "</h2>"),
+    H3("<h3>", "</h3>"),
+    H4("<h4>","</h4>"),
+    H5("<h5>", "</h5>"),
+    H6("<h6>", "</h6>"),
+    B("<b>", "</b>"),
+    I("<i>", "</i>"),
+    U("<u>", "</u>"),
+    TT("<tt>", "</tt>"),
+    CITE("<cite>", "</cite>"),
+    EM("<em>", "</em>"),
+    P("<p>", "</p>"),
+    STRONG("<strong>","</strong>"),
+    DL("<dl>", "</dl>"),
+    OL("<ol>", "</ol>"),
+    UL("<ul>", "</ul>"),
+    LI("<li>", "</li>"),
+    TABLE("<table>", "</table>"),
+    TR("<tr>", "</tr>"),
+    TD("<td>", "</td>"),
+    TH("<th>", "</th>"),
+    FORM("<form>", "</form>"),
+    FRAMESET("<frameset>", "</frameset>"),
+    NOFRAMES("<noframes>", "</noframes>"),
 
-    a("<a ", "href=", "</a>"), img("<img ", "src=", null);
+    a("<a ", "href=", "</a>"),
+    img("<img ", "src=", null);
 
     /**
      * BR("<br>
